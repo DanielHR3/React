36d95c089c8afb9aab326d58dee910e8d24af836
DanielHR3 <danielhrubio3@gmail.com>
2025-08-13 19:58:33 -0600

    exports

--- 01-reforzamiento/src/bases/08-imp-exp.ts
+++ 01-reforzamiento/src/bases/08-imp-exp.ts
@@ -1,7 +1,12 @@
-import { operators } from "../data/operators.data";
+import { operators, Operator } from "../data/operators.data";
 
+const getOperators = (id: number): Operator | undefined => {
+  const operator = operators.find((operator) => {
+    return operator.id === id;
+  });
+  if (!operator) {
+    throw new Error(`No existe un operador`);
+  }
 
-
-const getOperators = () => {
-  return operators;
+  return operator;
 };
--- 01-reforzamiento/src/data/operators.data.ts
+++ 01-reforzamiento/src/data/operators.data.ts
@@ -1,40 +1,40 @@
-
-interface Operator {
-    id: number;
-    codename: string;
-    unit: Unit;
+export interface Operator {
+  id: number;
+  codename: string;
+  unit: Unit;
 }
 
 //type Unit = 'DEVGRU' | 'SEAL Team 6';
 enum Unit {
-    DEVGRU = 'DEVGRU',
-    SEAL_Team_6 = 'SEAL Team 6',
+  DEVGRU = "DEVGRU",
+  SEAL_Team_6 = "SEAL Team 6",
 }
 
 export const operators: Operator[] = [
   {
     id: 1,
-    codename: 'Shadow-1',
+    codename: "Shadow-1",
     unit: Unit.DEVGRU,
   },
   {
     id: 2,
-    codename: 'Viper',
+    codename: "Viper",
     unit: Unit.SEAL_Team_6,
   },
   {
     id: 3,
-    codename: 'Ghost',
+    codename: "Ghost",
     unit: Unit.SEAL_Team_6,
   },
   {
     id: 4,
-    codename: 'Reaper',
+    codename: "Reaper",
     unit: Unit.DEVGRU,
   },
   {
     id: 5,
-    codename: 'Havoc',
+    codename: "Havoc",
     unit: Unit.SEAL_Team_6,
   },
 ] as const;
+//export default operators;
